b17e75036550baaaf3d9b75282d5f5caafd57a8c
MiguelADSilva <miguelaozinho9910@gmail.com>
2022-08-31 23:04:05 +0100

    refactor

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 
-import { ContainerNavBar, Title, ContentMenu } from './Styles'
+import { 
+  ContainerNavBar, 
+  Title, 
+  ContentMenu, 
+  DivSearch, 
+  SearchIcon, 
+  InputSearch,
+  DivInput
+} from './Styles'
 
 import { AiOutlineSearch } from 'react-icons/ai'
+import { BiMenu } from 'react-icons/bi'
 
 const NavBar = () => {
+
   return (
     <ContainerNavBar>
         <Title>FootShorts</Title>
         <ContentMenu>
-          <AiOutlineSearch />
+          <DivSearch>
+            <SearchIcon>
+              <DivInput>
+                <InputSearch type="text" placeholder="Search...."/>
+                <AiOutlineSearch />
+              </DivInput>
+            </SearchIcon>
+          </DivSearch>
+          <BiMenu />
         </ContentMenu>
     </ContainerNavBar>
   )
--- src/Components/NavBar/Styles.js
+++ src/Components/NavBar/Styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { AiOutlineSearch } from 'react-icons/ai'
+
 export const ContainerNavBar = styled.div `
     margin-bottom: 2rem !important;
     display: flex;
@@ -9,7 +11,6 @@ export const ContainerNavBar = styled.div `
     min-height: 7rem;
     position: sticky;
     top: 3rem;
-    background-color: red;
     align-items: center;
     justify-content: space-between;
 `
@@ -20,4 +21,48 @@ export const Title = styled.div `
   font-size: 5rem;
 `
 
-export const ContentMenu = styled.div ``
+export const ContentMenu = styled.div `
+    display: flex;
+    flex-direction: row;
+    min-width: 10vw !important;
+    justify-content: space-around;
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivSearch = styled.div `
+  min-width: 5vw !important;
+  display: flex;
+  flex-direction: row; 
+`
+
+export const DivInput = styled.div `
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const InputSearch = styled.input `
+    visibility: hidden;
+    font-size: 2rem;
+    height: 3rem !important;
+    border-radius: 5rem !important;
+    border: transparent;
+    padding-left: 1rem !important;
+
+    &::placeholder  {
+        padding-left: 1rem !important;
+    }
+`
+
+export const SearchIcon = styled.i `
+    cursor: pointer;
+
+    &:hover {
+        color: black;
+    }
+
+    &:hover ${InputSearch} {
+        visibility: visible;
+    }
+`
